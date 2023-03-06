2c8c192694b0bf5f19bfc21836d2477bac78677e
Matej Krenek <kre39158@lp608.spseol.cz>
2023-03-06 08:33:46 +0100

    add gui application starting structure

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Ahoj ňoumo!")
+import tkinter as tkinter
+
+class GUIApplication(tkinter.Tk):
+    def __init__(self, title):
+        super().__init__()
+        self.title(title)
+
+
+if __name__ == "__main__":
+    app = GUIApplication("tkGraf")
+    app.mainloop()
